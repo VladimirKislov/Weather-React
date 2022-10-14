6d4f74fe85e212fd95d9ffee489ecdcb0134c0d6
VladimirKislov <n_70@inbox.ru>
2022-10-15 02:13:32 +0800

    add port

--- src/server/server.js
+++ src/server/server.js
@@ -5,6 +5,7 @@ import helmet from "helmet";
 import { indexTemplate } from "./indexTemplate";
 import { App } from "../App";
 
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use("/static", express.static("./dist/client"));
@@ -21,6 +22,6 @@ app.get("/", (req, res) => {
   );
 });
 
-app.listen(3000, () => {
-  console.log("Server started on http://localhost:3000");
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
 });
